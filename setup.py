8df0115356aba9f6544f584861e488bba2bd5d9e
Ivan Metzlar <metzlar@gmail.com>
2014-01-03 15:10:00 -0800

    small change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup # pragma: no cover
 
 setup(
     name='Django Period Widget',
-    py_modules=['django_period_widget',],
+    py_modules=['django_period_widget'],
     cmdclass={'upload':lambda x:None},
     install_requires=[
         'django',
